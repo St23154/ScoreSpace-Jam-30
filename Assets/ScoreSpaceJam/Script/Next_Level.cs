c6ed651a0143611f331816610a829eb120340f2c
EmilienB <emiliendollar@gmail.com>
2024-06-23 16:04:18 +0200

    azerty

--- Assets/ScoreSpaceJam/Script/Next_Level.cs
+++ Assets/ScoreSpaceJam/Script/Next_Level.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Next_Level : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.CompareTag("Player")){
+            Scene_Controller.instance.NextLevel();
+        }
     }
 }
---
+++ Assets/ScoreSpaceJam/Script/Scene_Controller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Controller : MonoBehaviour
+{
+    public static Scene_Controller instance;
+    private void Awake(){
+        if(instance==null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    public void NextLevel(){
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex +1 );
+    }
+}
