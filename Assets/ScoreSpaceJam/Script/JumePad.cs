c0dffaf1b76ee235f75370001e69078105839fa1
EmilienB <emiliendollar@gmail.com>
2024-06-23 12:48:47 +0200

    tt les game macanique marchent

--- Assets/ScoreSpaceJam/Script/JumePad.cs
+++ Assets/ScoreSpaceJam/Script/JumePad.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class JumePad : MonoBehaviour
 {
+    public float bounce = 40f;
     // Start is called before the first frame update
-    void Start()
+   private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("moveable_object"))
+        {
+            Debug.Log("zert");
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+    
+        }
     }
 }
