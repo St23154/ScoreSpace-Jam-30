7b591d06dd0c87d359763ffb9718a7a523f91123
St23154 <e2peyrot@gmail.com>
2024-06-24 02:16:48 +0200

    transition qui marche ap

--- Assets/ScoreSpaceJam/Script/Scene_Controller.cs
+++ Assets/ScoreSpaceJam/Script/Scene_Controller.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Scene_Controller : MonoBehaviour
 {
+    [SerializeField] private GameObject _startingSceneTransition;
+    [SerializeField] private GameObject _endingSceneTransition;
     public static Scene_Controller instance;
     private void Awake(){
         if(instance==null){
@@ -16,6 +18,14 @@ public class Scene_Controller : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        _startingSceneTransition = GameObject.FindWithTag("startscene").GetComponent<GameObject>();
+        _endingSceneTransition = GameObject.FindWithTag("endscene").GetComponent<GameObject>();
+        _endingSceneTransition.SetActive(true);
+    }
+
+
    void Update(){
         if(Input.GetKeyUp(KeyCode.R)){
             SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
@@ -23,6 +33,7 @@ public class Scene_Controller : MonoBehaviour
     }
     // Start is called before the first frame update
     public void NextLevel(){
+        _startingSceneTransition.SetActive(true);
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex +1 );
     }
 }
---
+++ Assets/ScoreSpaceJam/Script/transition/StartTransition.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTransition : MonoBehaviour
+{
+    public void DisableAnimation()
+    {
+        gameObject.SetActive(false);
+    }
+}
